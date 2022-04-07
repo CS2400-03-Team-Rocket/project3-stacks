2e459a6ff218cc91af8c580bda8cdc7f231cf34b
Eric Huang <erichuang54@gmail.com>
2022-04-06 23:15:16 -0700

    added unit tests for getHeight & get#ofNodes

--- Task1Test.java
+++ Task1Test.java
@@ -56,4 +56,54 @@ public class Task1Test extends BinaryTree
         assertEquals("4527631", outContent.toString());
 
     }
+
+    @Test
+    public void testGetHeight()
+    {
+
+        BinaryTree<Integer> mockBinaryTree = new BinaryTree<>();
+        // Leaves
+		BinaryTree<Integer> dTree = new BinaryTree<>(4);
+		BinaryTree<Integer> eTree = new BinaryTree<>(5);
+		BinaryTree<Integer> gTree = new BinaryTree<>(7);
+
+		// Subtrees:
+		BinaryTree<Integer> fTree = new BinaryTree<>(6, null, gTree);
+		BinaryTree<Integer> bTree = new BinaryTree<>(2, dTree, eTree);
+		BinaryTree<Integer> cTree = new BinaryTree<>(3, fTree, null);
+
+		mockBinaryTree.setTree(1, bTree, cTree);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        mockBinaryTree.getHeight();
+        //height = 4
+        assertEquals("4", outContent.toString());
+    }
+
+    @Test
+    public void testGetNumberOfNodes()
+    {
+
+        BinaryTree<Integer> mockBinaryTree = new BinaryTree<>();
+        // Leaves
+		BinaryTree<Integer> dTree = new BinaryTree<>(4);
+		BinaryTree<Integer> eTree = new BinaryTree<>(5);
+		BinaryTree<Integer> gTree = new BinaryTree<>(7);
+
+		// Subtrees:
+		BinaryTree<Integer> fTree = new BinaryTree<>(6, null, gTree);
+		BinaryTree<Integer> bTree = new BinaryTree<>(2, dTree, eTree);
+		BinaryTree<Integer> cTree = new BinaryTree<>(3, fTree, null);
+
+		mockBinaryTree.setTree(1, bTree, cTree);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        mockBinaryTree.getNumberOfNodes();
+        //height = 7
+        assertEquals("7", outContent.toString());
+    }
 }
